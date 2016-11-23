5295c8cdf52ab7ddf9fd52ad14b554bc35f9e206
Vadim <morozovvadim91@gmail.com>
2016-11-23 18:48:30 +0200

    Start work with empty view #8

--- app/src/main/java/com/fentury/testapp/activity/MainActivity.java
+++ app/src/main/java/com/fentury/testapp/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fentury.testapp.R;
@@ -26,6 +28,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.card_recycler_view)
     RecyclerView recyclerView;
+    @BindView(R.id.empty_view)
+    TextView emptyView;
     private TopStoriesAdapter topStoriesAdapter;
     private List<Integer> topStories = new ArrayList<>();
     private Retrofit retrofit;
@@ -54,6 +58,13 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         topStoriesAdapter = new TopStoriesAdapter(models, this);
         recyclerView.setAdapter(topStoriesAdapter);
+        if (topStories.isEmpty()) {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        } else {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        }
     }
 
     private void loadJSON() {
