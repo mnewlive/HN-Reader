95ee005cc03bec8a118b3b267a9d99315eb03f98
Vadimm <vadimm@saltedge.com>
2016-12-05 18:07:05 +0200

    Changed class on Kotlin

---
+++ app/src/main/java/com/fentury/testapp/utils/StringFormatter.kt
@@ -0,0 +1,25 @@
+package com.fentury.testapp.utils
+
+import android.content.Context
+import android.content.res.Resources
+
+/**
+ * Created by morozov on 12/5/16.
+ */
+class StringFormatter(val context: Context) {
+
+    fun formatString(resId: Int, str: String?): String {
+        var result1 = ""
+
+        try {
+            result1 = context.getString(resId)
+        } catch (e: Resources.NotFoundException) {
+
+        }
+
+        /* Elvis operator
+        val str1 = str ?: ""
+        */
+        return String.format("%s %s", result1, str ?: "" ).trim { it <= ' ' }
+    }
+}
